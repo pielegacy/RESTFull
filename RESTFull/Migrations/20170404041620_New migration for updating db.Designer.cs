963de52b2174bcc596c539161845e0037d2b139a
Alex Billson <strikingmongoose@hotmail.com>
2017-04-04 14:20:34 +1000

    Added migrations

--- RESTFull/Migrations/20170404041620_New migration for updating db.Designer.cs
+++ RESTFull/Migrations/20170404041620_New migration for updating db.Designer.cs
@@ -9,9 +9,10 @@ using RESTFull.Models;
 namespace RESTFull.Migrations
 {
     [DbContext(typeof(Db))]
-    partial class DbModelSnapshot : ModelSnapshot
+    [Migration("20170404041620_New migration for updating db")]
+    partial class Newmigrationforupdatingdb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1");
---
+++ RESTFull/Migrations/20170404041620_New migration for updating db.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RESTFull.Migrations
+{
+    public partial class Newmigrationforupdatingdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Discount",
+                table: "MenuItems",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "MenuItems");
+        }
+    }
+}
