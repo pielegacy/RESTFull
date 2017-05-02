d32e9deaddc0b2ace660869e4ac068931311f9dd
Kevin <100995180@student.swin.edu.au>
2017-05-02 18:49:36 +1000

    Fixing Description in MenuItem Unit Testing

--- TESTFull/MenuItemTests.cs
+++ TESTFull/MenuItemTests.cs
@@ -10,11 +10,12 @@ namespace TESTFull
         public void TestNewMenuItem()
         {
             MenuItem testMenuItem = new MenuItem() { Name = "Test Menu Item", Price = 10m, Type = MenuItemType.Main, Calories= 1000};
-            testMenuItem.Description = "";
+            testMenuItem.Description = "some description";
             Assert.AreEqual("Test Menu Item", testMenuItem.Name);
             Assert.AreEqual(10m, testMenuItem.Price);
             Assert.AreEqual(MenuItemType.Main, testMenuItem.Type);
             Assert.AreEqual(1000, testMenuItem.Calories);
+            Assert.AreEqual("some description", testMenuItem.Description);
         }
 
         [TestMethod]
