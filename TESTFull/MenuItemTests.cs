e802bb43cc8adcfc92a83b593753dfd603cb6e88
Kevin <100995180@student.swin.edu.au>
2017-05-02 13:25:08 +1000

    Store DEtails Unit TEsting

--- TESTFull/MenuItemTests.cs
+++ TESTFull/MenuItemTests.cs
@@ -6,13 +6,22 @@ namespace TESTFull
     [TestClass]
     public class MenuItemTests
     {
-        [TestMethod]        
+        [TestMethod]
         public void TestNewMenuItem()
         {
             MenuItem testMenuItem = new MenuItem() { Name = "Test Menu Item", Price = 10m, Type = MenuItemType.Main };
             Assert.AreEqual("Test Menu Item", testMenuItem.Name);
             Assert.AreEqual(10m, testMenuItem.Price);
-            Assert.AreEqual(MenuItemType.Main, testMenuItem.Type);            
+            Assert.AreEqual(MenuItemType.Main, testMenuItem.Type);
+        }
+
+        [TestMethod]
+        public void TestMenuItemRequired()
+        {
+
+            MenuItem testMenuItem = new MenuItem() { Price = 10m, Type = MenuItemType.Main };
+            if (testMenuItem.Name == null)
+                Assert.Fail();
         }
     }
 }
