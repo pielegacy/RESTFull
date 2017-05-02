0eb603f401f51b4e135a96cd97b56484f9d60dda
Kevin <100995180@student.swin.edu.au>
2017-05-02 14:11:04 +1000

    add Combo Unit Testing

---
+++ TESTFull/ComboTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTFull.Models;
+
+namespace TESTFull
+{
+    [TestClass]
+    public class ComboTest
+    {
+        [TestMethod]
+        public void TestComboContent()
+        {
+            Combo Com = new Combo() { ComboDescription= "Some description", ComboPrice= 9.8m};
+            Assert.AreEqual("Some description", Com.ComboDescription);
+            Assert.AreEqual(9.8m, Com.ComboPrice);
+        }
+
+        [TestMethod]
+        public void TestComboRequired()
+        {
+
+            Combo Com = new Combo() { ComboPrice = 10m};
+            if (Com.ComboDescription == null)
+                Assert.Fail();
+        }
+    }
+}
