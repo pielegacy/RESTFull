e296528d303c8056d41acdb58ab3c3bf5b29fd95
Kevin <100995180@student.swin.edu.au>
2017-05-02 18:08:13 +1000

    Authentication Unit Testing

---
+++ TESTFull/AuthenticationTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTFull.Models;
+
+namespace TESTFull
+{
+    [TestClass]
+    public class AuthenticationTest
+    {
+        [TestMethod]
+        public void TestAuthenticationContent()
+        {
+            Authentication At = new Authentication() { UserName = "Name1", Password = "Password" };            
+            Assert.AreEqual("Name1", At.UserName);
+            Assert.AreEqual("Password", At.Password);
+        }
+
+        [TestMethod]
+        public void TestMenuItemRequired()
+        {
+            Authentication At = new Authentication();
+            if (At.UserName == null)
+                Assert.Fail();
+            if (At.Password == null)
+                Assert.Fail();
+        }
+    }
+}
