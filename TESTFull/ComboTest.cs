773edb9409cac990f55babf40ae780e8ad6740b1
Kevin <100995180@student.swin.edu.au>
2017-05-08 11:30:43 +1000

    Add Link between main menu to tag and combo

--- TESTFull/ComboTest.cs
+++ TESTFull/ComboTest.cs
@@ -10,6 +10,10 @@ namespace TESTFull
         public void TestComboContent()
         {
             Combo Com = new Combo() { ComboDescription= "Some description", ComboPrice= 9.8m};
+            MenuItem Pizza=new MenuItem(){Name="Pizza", Price=8.8m, Type=MenuItemType.Main};
+            Com.Items.Add(Pizza);
+
+            Assert.AreSame(Pizza,Com.Items[0]);
             Assert.AreEqual("Some description", Com.ComboDescription);
             Assert.AreEqual(9.8m, Com.ComboPrice);
 
--- TESTFull/TagTest.cs
+++ TESTFull/TagTest.cs
@@ -12,8 +12,11 @@ namespace TESTFull
         [DataRow("Chicken", "Fried Chicken")]
         public void TestNewMenuItem(string a, string b)
         {
-            Tag T = new Tag() { TagName = a, MenuName = b};            
+            Tag T = new Tag() { TagName = a};
+            MenuItem food=new MenuItem(){Name=b, Price=8.8m, Type=MenuItemType.Main};
+            T.Items.Add(food);
 
+            Assert.AreSame(food,T.Items[0]);           
             Assert.AreEqual(a, T.TagName);
             Assert.AreEqual(b, T.MenuName);
 
