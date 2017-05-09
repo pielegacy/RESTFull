211f7fcf54ecc8dee9e1fe2c75f479636ef75570
Alex Billson <strikingmongoose@hotmail.com>
2017-05-09 10:06:25 +1000

    Added MenuItem description database migration

---
+++ RESTFull/Migrations/20170509000452_MenuItem contains description.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RESTFull.Migrations
+{
+    public partial class MenuItemcontainsdescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "MenuItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "MenuItems");
+        }
+    }
+}
