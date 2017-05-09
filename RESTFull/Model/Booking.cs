ef15c3a29408e52d6d28b95be99d52c3947d015a
Alex Billson <strikingmongoose@hotmail.com>
2017-05-09 10:43:51 +1000

    Implemented booking model in relation to unit tests

---
+++ RESTFull/Model/Booking.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace RESTFull.Models
+{
+    ///<summary>
+    /// Bookings encompass the table logic of the restaurant.
+    ///</summary>
+    public class Booking
+    {
+        ///<summary>
+        /// The name of the Store which the booking relates to.
+        ///</summary>
+        public string StoreName { get; set; }
+        ///<summary>
+        /// The table number
+        ///</summary>
+        public int Number { get; set; }
+        ///<summary>
+        /// The capacity of the table, used to figure out if available is valid
+        ///</summary>
+        public int TableCapacity { get; set; }
+        ///<summary>
+        /// The amount that's been booked
+        ///</summary>
+        ///<remarks>
+        /// Might change name of this, I don't like the structure
+        ///</remarks>
+        public int Available { get; set; }
+    }
+}
--- TESTFull/BookingTest.cs
+++ TESTFull/BookingTest.cs
@@ -3,43 +3,43 @@ using RESTFull.Models;
 
 namespace TESTFull
 {
-    // Uncomment when implemented
-    //[TestClass]
-    //public class BookingTest
-    //{
-    //    [TestMethod]
-    //    [DataRow(50,20,"Branch1")]
-    //    public void TestBookingContent(int cap, int able, string name)
-    //    {
-    //        Booking Book = new Booking() { TableCapacity = cap, Available = able, StoreName= name};
+   [TestClass]
+    public class BookingTest
+    {
+        [TestMethod]
+        [DataRow(1, 50, 20, "Branch1")]
+        public void TestBookingContent(int number, int cap, int able, string name)
+        {
+            Booking Book = new Booking() { Number = number, TableCapacity = cap, Available = able, StoreName = name };
 
-    //        Assert.AreEqual(cap, Book.TableCapacity);
-    //        Assert.AreEqual(able, Book.Available);
-    //        Assert.AreEqual(name, Book.StoreName);
-    //        if (Book.TableCapacity < Book.Available)
-    //        {
-    //            Assert.Fail();
-    //        }
+            Assert.AreEqual(number, Book.Number);
+            Assert.AreEqual(cap, Book.TableCapacity);
+            Assert.AreEqual(able, Book.Available);
+            Assert.AreEqual(name, Book.StoreName);
+            if (Book.TableCapacity < Book.Available)
+            {
+                Assert.Fail();
+            }
 
-    //        //Required Attribute
-    //        Assert.IsNotNull(Book.TableCapacity);
-    //        Assert.IsNotNull(Book.Available);
-    //        Assert.IsNotNull(Book.StoreName);
-    //    }
+            //Required Attribute
+            Assert.IsNotNull(Book.TableCapacity);
+            Assert.IsNotNull(Book.Available);
+            Assert.IsNotNull(Book.StoreName);
+        }
 
-    //    [TestMethod]
-    //    [DataRow(null, 10, "")]
-    //    [DataRow(78, null, "branch")]
-    //    [DataRow(78, 22, null)]
-    //    public void TestBookingRequired(int cap, int able, string name)
-    //    {
-    //        Booking Book = new Booking() { TableCapacity = cap, Available = able, StoreName = name };
-    //        if (Book.TableCapacity == null)
-    //            Assert.Fail();
-    //        if (Book.Available == null)
-    //            Assert.Fail();
-    //        if (Book.StoreName == null)
-    //            Assert.Fail();
-    //    }
-    //}
+        [TestMethod]
+        [DataRow(3, 2, 10, "branch")]
+        [DataRow(1, 78, 3, "branch")]
+        [DataRow(2, 78, 22, "branch")]
+        public void TestBookingRequired(int number, int cap, int able, string name)
+        {
+            Booking Book = new Booking() { Number = number, TableCapacity = cap, Available = able, StoreName = name };
+            if (Book.TableCapacity == null)
+                Assert.Fail();
+            if (Book.Available == null)
+                Assert.Fail();
+            if (Book.StoreName == null)
+                Assert.Fail();
+        }
+    }
 }
