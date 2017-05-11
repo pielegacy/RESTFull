1dd5c3bd1901d25ab04a19d18c0ac9aa38402a41
Alex Billson <strikingmongoose@hotmail.com>
2017-05-11 13:22:27 +1000

    Working on the Booking implementations

--- RESTFull/Model/Booking.cs
+++ RESTFull/Model/Booking.cs
@@ -10,25 +10,13 @@ namespace RESTFull.Models
     {
         [Key]
         public int Id { get; set; }
-
         ///<summary>
-        /// The name of the Store which the booking relates to.
+        /// The name which the booking is made under
         ///</summary>
-        public string StoreName { get; set; }
+        public string BookingName { get; set; }
         ///<summary>
-        /// The table number
+        /// The date which the booking is made under
         ///</summary>
-        public int Number { get; set; }
-        ///<summary>
-        /// The capacity of the table, used to figure out if available is valid
-        ///</summary>
-        public int TableCapacity { get; set; }
-        ///<summary>
-        /// The amount that's been booked
-        ///</summary>
-        ///<remarks>
-        /// Might change name of this, I don't like the structure
-        ///</remarks>
-        public int Available { get; set; }
+        public DateTime Date { get; set; }
     }
 }
--- TESTFull/BookingTest.cs
+++ TESTFull/BookingTest.cs
@@ -7,34 +7,34 @@ namespace TESTFull
    [TestClass]
     public class BookingTest
     {
-        [TestMethod]
-        [DataRow(1, 50, 20, "Branch1")]
-        public void TestBookingContent(int number, int cap, int able, string name)
-        {
-            Booking Book = new Booking() { Number = number, TableCapacity = cap, Available = able, StoreName = name };
+        //[TestMethod]
+        //[DataRow(1, 50, 20, "Branch1")]
+        //public void TestBookingContent(int number, int cap, int able, string name)
+        //{
+        //    Booking Book = new Booking() { Number = number, TableCapacity = cap, Available = able, StoreName = name };
 
-            Assert.AreEqual(number, Book.Number);
-            Assert.AreEqual(cap, Book.TableCapacity);
-            Assert.AreEqual(able, Book.Available);
-            Assert.AreEqual(name, Book.StoreName);
-            if (Book.TableCapacity < Book.Available)
-            {
-                Assert.Fail();
-            }
-        }
+        //    Assert.AreEqual(number, Book.Number);
+        //    Assert.AreEqual(cap, Book.TableCapacity);
+        //    Assert.AreEqual(able, Book.Available);
+        //    Assert.AreEqual(name, Book.StoreName);
+        //    if (Book.TableCapacity < Book.Available)
+        //    {
+        //        Assert.Fail();
+        //    }
+        //}
 
-        [TestMethod]
-        [DataRow(3, 2, 10, "branch")]
-        [DataRow(1, 78, 3, "branch")]
-        [DataRow(2, 78, 22, "branch")]
-        public void TestBookingRequired(int number, int cap, int able, string name)
-        {
-            Booking Book = new Booking() { Number = number, TableCapacity = cap, Available = able, StoreName = name };
+        //[TestMethod]
+        //[DataRow(3, 2, 10, "branch")]
+        //[DataRow(1, 78, 3, "branch")]
+        //[DataRow(2, 78, 22, "branch")]
+        //public void TestBookingRequired(int number, int cap, int able, string name)
+        //{
+        //    Booking Book = new Booking() { Number = number, TableCapacity = cap, Available = able, StoreName = name };
            
-            Assert.IsNotNull(Book.TableCapacity);
-            Assert.IsNotNull(Book.Available);
-            Assert.IsNotNull(Book.StoreName);
-        }
+        //    Assert.IsNotNull(Book.TableCapacity);
+        //    Assert.IsNotNull(Book.Available);
+        //    Assert.IsNotNull(Book.StoreName);
+        //}
 
         //after some consideration this is the best layout for booking
         [TestMethod]
