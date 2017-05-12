cd759ade5db8abb6b65b14bcbbb2a614700deff3
Kevin <100995180@student.swin.edu.au>
2017-05-12 11:20:42 +1000

    Testing tag redundacy

--- TESTFull/TagTest.cs
+++ TESTFull/TagTest.cs
@@ -11,25 +11,33 @@ namespace TESTFull
         [DataRow("Veggie", "Caesar Salad")]
         [DataRow("Chicken", "Fried Chicken")]
         public void TestNewMenuItem(string a, string b)
-        {
+        {                      
             Tag T = new Tag() { TagName = a};
             MenuItem food=new MenuItem(){Name=b, Price=8.8m, Type=MenuItemType.Main};
             T.Items.Add(food);
 
-            Assert.AreSame(food,T.Items[0]);           
+            Assert.AreSame(food,T.Items[0]);      
             Assert.AreEqual(a, T.TagName);
             Assert.AreEqual(b, T.MenuName);
 
             //Required Attribute
             Assert.IsNotNull(T.TagName);
-        }
+        }    
 
         [TestMethod]
-        public void TestMenuItemRequired()
-        {            
-            Tag T = new Tag() { MenuName="SomeMenu"};
+        public void TestTagRedundancy(string origin, string redundant)
+        {
+            Tag T1= new Tag() {TagName= origin};
+            Tag T2 = new Tag() {TagName= redundant, MenuName = "Something"};
 
-            Assert.IsNotNull(T.TagName);
+            T1.TagName= T1.TagName.ToLower();
+            T2.TagName= T2.TagName.ToLower();
+
+            if (T2.TagName == (T1.TagName + " ") || T2.TagName == (" " + T1.TagName) || T2.TagName == T1.TagName)
+            {
+                T1.Items.Add(T2.MenuName);
+                TagController.Delete(T2.id);
+            }            
         }
     }
 }
