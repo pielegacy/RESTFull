19809538bb8d1c65e34f99a3d74af6256fd37d6f
pielegacy <alex_billson@outlook.com>
2017-05-14 11:36:05 +1000

    Implemented all Discount CRUD operations

--- RESTFull/Controllers/DiscountsController.cs
+++ RESTFull/Controllers/DiscountsController.cs
@@ -10,7 +10,7 @@ namespace RESTFull.Controllers
     [Route("api/[controller]")]
     public class DiscountsController : Controller
     {
-        // GET api/MenuItems?name=example name
+        // GET api/Discounts
         [HttpGet]
         public List<Discount> Get()
         {
@@ -20,42 +20,69 @@ namespace RESTFull.Controllers
             return result;
         }
 
-        // GET api/MenuItems/5
+        // GET api/Discounts/5
         [HttpGet("{id}")]
-        public MenuItem Get(int id)
+        public Discount Get(int id)
         {
             // Use the database object
             using (var db = new Db())
             {
                 // Return only the item which has the same id
-                return db.MenuItems.FirstOrDefault(m => m.Id == id);
+                return db.Discounts.FirstOrDefault(m => m.Id == id);
             }
         }
 
-        // POST api/MenuItems
+        // POST api/Discounts
         [HttpPost]
-        public async void Post([FromBody]Discount value)
+        public async Task<ActionResult> Post([FromBody]Discount value)
         {
             // Use the database object
             using (var db = new Db())
             {
                 if (ModelState.IsValid && value.DiscountPercentage != 0)
+                {
                     db.Discounts.Add(value);
-                // Save the changes without clogging up the main thread
-                await db.SaveChangesAsync();
+                    // Save the changes without clogging up the main thread
+                    await db.SaveChangesAsync();
+                    return new CreatedAtRouteResult("Discounts", value.Id);
+                }
+                else
+                    return new StatusCodeResult(400);
             }
         }
 
-        // PUT api/MenuItems/5
+        // PUT api/Discounts/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<ActionResult> Put(int id, [FromBody]Discount value)
         {
+            // Use the database object
+            using (var db = new Db())
+            {
+                Discount res = db.Discounts.FirstOrDefault(d => d.Id == id);
+                db.Discounts.Remove(res);
+                if (ModelState.IsValid && value.DiscountPercentage != 0)
+                {
+                    db.Discounts.Add(value);
+                    // Save the changes without clogging up the main thread
+                    await db.SaveChangesAsync();
+                    return new CreatedAtRouteResult("Discounts", value.Id);
+                }
+                else
+                    return new StatusCodeResult(400);
+            }
         }
 
-        // DELETE api/MenuItems/5
+        // DELETE api/Discounts/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<Discount> Delete(int id)
         {
+            using (var db = new Db())
+            {
+                Discount removed = db.Discounts.FirstOrDefault(d => d.Id == id);
+                db.Discounts.Remove(removed);
+                await db.SaveChangesAsync();
+                return removed;
+            }
         }
     }
 }
