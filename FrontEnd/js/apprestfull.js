ed88af4d7a3a6217e24307d427fa68254f5ea682
pielegacy <alex_billson@outlook.com>
2017-05-16 20:24:15 +1000

    Added posting for discounts

--- FrontEnd/js/apprestfull.js
+++ FrontEnd/js/apprestfull.js
@@ -1,64 +1,73 @@
 var app = angular.module('App', ['ngRoute']);
 
-app.config(function($routeProvider,$locationProvider){
+app.config(function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
 
   $routeProvider
-    .when('/menulist',{
+    .when('/menulist', {
       templateUrl: 'views/menulist.html',
       controller: 'menulistCtrl'
-  })
-    .when('/additem',{
+    })
+    .when('/additem', {
       templateUrl: 'views/additem.html',
       controller: 'menulistCtrl'
-  }).when('/restaurantinfo',{
+    }).when('/restaurantinfo', {
       templateUrl: 'views/restaurantinfo.html',
       controller: 'menulistCtrl'
-  }).otherwise({
-    redirectTo: '/menulist'
-  });
+    }).otherwise({
+      redirectTo: '/menulist'
+    });
 
 });
 
 
-app.controller('menulistCtrl',function($scope, $http, $timeout) {
+app.controller('menulistCtrl', function ($scope, $http, $timeout) {
   //variables
   $scope.addSuccess = false;
-  
+
   var jsonLink = "http://li648-103.members.linode.com/api/MenuItems";
-  $http.get(jsonLink).then(function(data){
+  $http.get(jsonLink).then(function (data) {
     $scope.menu = data.data;
     //console.log(JSON.stringify($scope.food));
   });
-  $http.get("http://li648-103.members.linode.com/api/Tags").then(function(data)
-  {
+  $http.get("http://li648-103.members.linode.com/api/Tags").then(function (data) {
     var resultArray = [];
-    for (let i of data.data)
-      {
-        resultArray.push(i.tagName);
-      }
+    for (let i of data.data) {
+      resultArray.push(i.tagName);
+    }
     $scope.tagNames = resultArray;
     console.log($scope.tagNames);
   });
-  $scope.deleteitem = function(item,index){
+  $scope.deleteitem = function (item, index) {
     //DELETE li648-103.members.linode.com/api/MenuItems/item.id
   };
 
-  $scope.additem = function(){
-    $http.post("http://li648-103.members.linode.com/api/MenuItems", JSON.stringify($scope.newItem)).then(function(data){
-      console.log("DATA SENT SUCCESSFULLY!");
-      $scope.addSuccess = true;
-      $timeout(function () { $scope.addSuccess = false; }, 3000);
-    });
+  $scope.additem = function () {
+    if ($scope.newDiscount.description.length != 0 && $scope.newDiscount.discountPercentage.length != 0) {
+      $http.post("http://li648-103.members.linode.com/api/Discounts", JSON.stringify($scope.newDiscount)).then(function (data) {
+        $scope.newItem.discountId = data.data;
+        $http.post("http://li648-103.members.linode.com/api/MenuItems", JSON.stringify($scope.newItem)).then(function (data) {
+          console.log("DATA SENT SUCCESSFULLY!");
+          $scope.addSuccess = true;
+          $timeout(function () { $scope.addSuccess = false; }, 3000);
+        });
+      });
+    }
+    else {
+      $http.post("http://li648-103.members.linode.com/api/MenuItems", JSON.stringify($scope.newItem)).then(function (data) {
+        console.log("DATA SENT SUCCESSFULLY!");
+        $scope.addSuccess = true;
+        $timeout(function () { $scope.addSuccess = false; }, 3000);
+      });
+    }
   };
-  
-  
 
-  $scope.testApplication = function()
-  {
+
+
+  $scope.testApplication = function () {
     // testing code appears here
   };
-  
+
 });
 
 
